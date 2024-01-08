95eb1d7b10bb83ac034e65f388e47b5644908dbf
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2024-01-07 17:53:00 -0800

    Completed form exercise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -7,10 +15,24 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_url, notice: "User created successfully"
+      redirect_to users_url, notice: "User created successfully"
 
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_url, notice: "User updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
