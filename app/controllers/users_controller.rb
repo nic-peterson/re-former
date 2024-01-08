e37022293d3d5744388bc191e175a35ea2a74544
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2024-01-07 17:22:51 -0800

    Added a #form_tag version to the new user view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to user, notice: "User created successfully"
+      redirect_to new_user_url, notice: "User created successfully"
     else
       render :new, status: :unprocessable_entity
     end
